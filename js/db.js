c92b34d2bcccd87a04303259583ed56630c3ae5c
Muhammad Arsalan <mohdarsalan318@gmail.com>
2019-08-13 17:43:22 +0500

    Dynammic UI

--- js/db.js
+++ js/db.js
@@ -2,6 +2,12 @@
 db.collection('recipes').onSnapshot(snapshot=>{
     //console.log(snapshot.docChanges());
     snapshot.docChanges().forEach(change => {
-        console.log(change,change.doc.data(),change.doc.id);
+        //console.log(change,change.doc.data(),change.doc.id);
+        if(change.type==='added'){
+            renderRecipe(change.doc.data(),change.doc.id);
+        }
+        if(change.type==='removed'){
+            
+        }
     });
 })
