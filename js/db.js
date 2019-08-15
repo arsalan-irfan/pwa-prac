bdf03700dad311ef01ff56fe7b53b4dd22a432a4
Muhammad Arsalan <mohdarsalan318@gmail.com>
2019-08-15 12:32:40 +0500

    Showing data in offline mode

--- js/db.js
+++ js/db.js
@@ -1,13 +1,26 @@
+//offline data
+db.enablePersistence()
+    .catch(err => {
+        if (error.code == 'failed-precondition') {
+            //probably multiple tabs open at once
+            console.log("Persistence Failed")
+        }
+        else if (err.code == 'unimplemented') {
+            console.log('Persistence is not available')
+        }
+    })
+
+
 //realtime listener
-db.collection('recipes').onSnapshot(snapshot=>{
+db.collection('recipes').onSnapshot(snapshot => {
     //console.log(snapshot.docChanges());
     snapshot.docChanges().forEach(change => {
         //console.log(change,change.doc.data(),change.doc.id);
-        if(change.type==='added'){
-            renderRecipe(change.doc.data(),change.doc.id);
+        if (change.type === 'added') {
+            renderRecipe(change.doc.data(), change.doc.id);
         }
-        if(change.type==='removed'){
-            
+        if (change.type === 'removed') {
+
         }
     });
 })
